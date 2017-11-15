85fac29fc99dcac286a5dac908ebef0d9a13f813
Tongjie <21030118+wangjieke@users.noreply.github.com>
2017-11-14 17:43:49 -0800

    Add feature now the program can count multiple .py files in a given directory

--- count.py
+++ count.py
@@ -10,56 +10,66 @@ def get_path() -> Path or None:
     """Get path
     :returns a Path object or None (when the user wants to quit the program)"""
     while True:
-        print('Please enter a file path; an empty input would make the program exit;')
+        print('Please enter a file path for counting single file;')
+        print('or enter a directory path for counting all .py files in that directory;')
+        print('an input containing only a dot "." would be considered as current work directory;')
+        print('an empty input would make the program exit;')
         path_str = input('Your input: ')
         if path_str == '':
             return None
-        file_path = Path(path_str)
-        if file_path.exists() and file_path.is_file():
-            return file_path
+        path = Path(path_str)
+        if path.exists():
+            return path
         else:
-            print('Invalid file path; Please retry.')
+            print('Invalid file path; please retry.')
             print()
 
 
-def count_lines(file_path: Path) -> None:
+def count_path(path: Path) -> dict:
+    result_dict = dict()
+    if path.is_file():
+        result_dict[str(path)] = count_single_file(path)
+    elif path.is_dir():
+        for py_file_path in list(path.glob('**/*.py')):
+            result_dict[str(py_file_path)] = count_single_file(py_file_path)
+    return result_dict
+
+
+def print_result(result_dict: dict) -> None:
+    for file in result_dict:
+        print('--------------------')
+        print(file)
+        for count in result_dict[file]:
+            print('The number of lines of {} is {}.'.format(count.split(sep='_')[0], result_dict[file][count]))
+
+
+def count_single_file(file_path: Path) -> dict:
     """Counting useful lines
-    :param file_path: user-input file path"""
-    file = None
-    try:
-        useful_line_sum, comment_line_sum, blank_line_sum, docstring_line_sum = 0, 0, 0, 0
-        docstring_continuing = False
-        file = file_path.open()
-        for line in file:
-            if line.strip().startswith('#'):
-                comment_line_sum += 1
-            elif line.strip() in '\r\n':
-                blank_line_sum += 1
-            elif line.strip().startswith('"""'):
-                docstring_line_sum += 1
-                if not line.strip().endswith('"""'):
-                    docstring_continuing = True
-            elif line.strip().endswith('"""') and docstring_continuing:
-                docstring_line_sum += 1
-                docstring_continuing = False
-            elif docstring_continuing:
-                docstring_line_sum += 1
-            else:
-                useful_line_sum += 1
-    except OSError:
-        print('Cannot open the file!')
-    else:
-        print('The sum of useful lines is {}.'.format(useful_line_sum))
-        print('The sum of comment lines is {}.'.format(comment_line_sum))
-        print('The sum of blank lines is {}.'.format(blank_line_sum))
-        print('The sum of docstring lines is {}.'.format(docstring_line_sum))
-        print('Total lines: {}'.format(useful_line_sum + comment_line_sum + blank_line_sum + docstring_line_sum))
-    finally:
-        if file is not None:
-            file.close()
-
-
-if __name__ == '__main__':
-    path = get_path()
-    if path is not None:
-        count_lines(path)
+    :param file_path: user-input file path
+    :return count of different types of lines as a dictionary"""
+    useful_line_sum, comment_line_sum, blank_line_sum, docstring_line_sum = 0, 0, 0, 0
+    docstring_continuing = False
+    file = file_path.open()
+    for line in file:
+        if line.strip().startswith('#'):
+            comment_line_sum += 1
+        elif line.strip() in '\r\n':
+            blank_line_sum += 1
+        elif line.strip().startswith('"""'):
+            docstring_line_sum += 1
+            if not line.strip().endswith('"""'):
+                docstring_continuing = True
+        elif line.strip().endswith('"""') and docstring_continuing:
+            docstring_line_sum += 1
+            docstring_continuing = False
+        elif docstring_continuing:
+            docstring_line_sum += 1
+        else:
+            useful_line_sum += 1
+    file.close()
+    return {
+        'useful_line_sum': useful_line_sum,
+        'comment_line_sum': comment_line_sum,
+        'blank_line_sum': blank_line_sum,
+        'docstring_line_sum': docstring_line_sum
+        }
---
+++ interface.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Created with PyCharm
+# @Time    : 11/14/17 5:07 PM
+# @Author  : Tongjie Wang
+
+import count
+
+
+def user_interface():
+    try:
+        path = count.get_path()
+        result_dict = count.count_path(path)
+        count.print_result(result_dict)
+    except OSError:
+        print('An error occurred when processing files.')
+
+
+if __name__ == '__main__':
+    user_interface()
